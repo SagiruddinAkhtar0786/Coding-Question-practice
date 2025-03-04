c034e31655337ccb8f347c43513c5689a6035f24
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-03-04 23:46:57 +0530

    COMMIT

--- Practice/src/digitalocean/com/java/programming/interview/questions/Factorial.java
+++ Practice/src/digitalocean/com/java/programming/interview/questions/Factorial.java
@@ -1,10 +1,22 @@
 package digitalocean.com.java.programming.interview.questions;
 
 public class Factorial {
+	
+	private static int findFactorial(int n) {
+		
+		int fact = 1;
+		for(int i = 1; i<=n ;i++) {
+			fact *= i;
+		}
+		return fact;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n = 5;
+		
+		int fact = findFactorial(n);
+		System.out.println("factorial of a number :: "+fact);
 	}
 
 }
