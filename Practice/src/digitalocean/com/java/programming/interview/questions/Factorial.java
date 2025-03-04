3895d46fae1d4b5a71430fe5dad98a2f45cf745e
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-03-04 23:44:37 +0530

    Commit

---
+++ Practice/src/digitalocean/com/java/programming/interview/questions/Factorial.java
@@ -0,0 +1,10 @@
+package digitalocean.com.java.programming.interview.questions;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
