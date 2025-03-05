640ad71f52e26ab01d13ddff7fd66ac9795b928b
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-03-06 00:29:22 +0530

    commit

--- Practice/src/com/thread/practice/ThreadMethodsAndConstructors/JoinMethodPauseMainThread.java
+++ Practice/src/com/thread/practice/ThreadMethodsAndConstructors/JoinMethodPauseMainThread.java
@@ -1,5 +1,19 @@
 package com.thread.practice.ThreadMethodsAndConstructors;
 
+/*class A extends Thread{
+	@Override
+    public void run() {
+        try {
+            JoinMethodPauseMainThread.mainThread.join(); // Waits for the main thread to finish
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Thread A executing :: " + Thread.currentThread().getName());
+        }
+    }
+}*/
 
 
 public class JoinMethodPauseMainThread extends Thread{
