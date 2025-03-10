e25628a51f119d92869278c384cf860fa085d8a5
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-03-10 11:56:47 +0530

    new changes done from my side

--- Practice/src/digitalocean/com/java/programming/interview/questions/BinarySearach.java
+++ Practice/src/digitalocean/com/java/programming/interview/questions/BinarySearach.java
@@ -38,6 +38,8 @@ public class BinarySearach {
 		return -1;
 	}
 
+	// new changes
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int arr[] = {1,2,3,4,5,6,7,8,9,10};
