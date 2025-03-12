29579a7e117faa7ec547d31e36d35bf7bc4c5b04
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-03-12 18:32:40 +0530

    commit

---
+++ Practice/src/digitalocean/com/java/programming/interview/questions/TextBlocks.java
@@ -0,0 +1,15 @@
+package digitalocean.com.java.programming.interview.questions;
+
+public class TextBlocks {
+//Java 15 added the text blocks feature. You can create multiline strings using text blocks. 
+	//The multiline string has to be
+	//written inside of a pair of triple-double quotes, as shown in the following example:
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*String  str = """ sagir
+							raju 
+							sali """;*/
+
+	}
+
+}
