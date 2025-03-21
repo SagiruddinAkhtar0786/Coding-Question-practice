ef92ef52cfca2cc54ef1a3ccad276712ca77a4ba
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-03-21 11:45:41 +0530

    commit

---
+++ Practice/src/com/practice/LeetCode/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,22 @@
+package com.practice.LeetCode;
+
+public class BestTimeToBuyAndSellStockII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int prices[] = new int[] {7,1,5,3,6,4};
+		int maxProfit = findMaxProfit(prices);
+		System.out.println("max Profit :: "+maxProfit);
+	}
+
+	private static int findMaxProfit(int[] prices) {
+		// TODO Auto-generated method stub
+		int maxProfit = 0;
+		for(int i = 1 ; i<prices.length;i++) {
+			if(prices[i] >prices[i-1])
+				maxProfit += (prices[i] - prices[i-1]);
+		}
+		return maxProfit;
+	}
+
+}
---
+++ Practice/src/com/practice/LeetCode/JumpGame.java
@@ -0,0 +1,30 @@
+package com.practice.LeetCode;
+
+public class JumpGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = new int[] { 2, 3, 1, 1, 4 };
+
+		boolean reached = reachToEndPoints(arr);
+		if (reached)
+			System.out.println("yes given arr will reach to end");
+		else
+			System.out.println("No given arr will not reach to end");
+
+	}
+
+	private static boolean reachToEndPoints(int[] arr) {
+		// TODO Auto-generated method stub
+		int reach = 0;
+		for (int i = 0; i <= reach; i++) {
+			reach = Math.max(reach, i + arr[i]);
+			if (reach >= arr.length) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+}
