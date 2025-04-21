7ba04e3f3ea251a43b8e7d8b91046da4d38867a3
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-04-21 21:07:38 +0530

    commit

--- Practice/src/com/practice/LeetCode/HashMap/repeatLeatzcode/RansomeNote.java
+++ Practice/src/com/practice/LeetCode/HashMap/repeatLeatzcode/RansomeNote.java
@@ -1,10 +1,29 @@
 package com.practice.LeetCode.HashMap.repeatLeatzcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RansomeNote {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String ransomeNote = "aabad" , magazine="aabad";
+		
+		Map<Character,Integer> count = new HashMap<>();
+		
+		for(char ch : magazine.toCharArray())
+			count.put(ch, count.getOrDefault(ch ,0)+1);
+		
+		for(char ch : ransomeNote.toCharArray()) {
+			if(!count.containsKey(ch) || count.get(ch) == 0 ) {
+				System.out.println("word can not be prepared ... ");
+				return;
+			}
+			
+			count.put(ch, count.get(ch)-1);
+		}
+		
+		System.out.println("words can be prepared...");
 	}
 
 }
