94541621aba20d29568bf87707b5bff945e4e841
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-04-26 09:28:25 +0530

    Commit

--- Practice/src/com/practice/LeetCode/SlidingWindow/MinimumSizeSubArraySum_Repeat.java
+++ Practice/src/com/practice/LeetCode/SlidingWindow/MinimumSizeSubArraySum_Repeat.java
@@ -1,5 +1,11 @@
 package com.practice.LeetCode.SlidingWindow;
 
+
+/*
+ Given an array of positive integers nums and a positive integer target, return 
+ the minimal length of a subarray whose sum is greater
+  than or equal to target. If there is no such subarray, return 0 instead.
+ */
 public class MinimumSizeSubArraySum_Repeat {
 
 	public static void main(String[] args) {
