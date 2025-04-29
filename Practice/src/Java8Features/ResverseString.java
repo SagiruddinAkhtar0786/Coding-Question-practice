ef49c9b4264824d0e31aa225139eaef9ff1ee8e3
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-04-29 22:22:04 +0530

    commit

---
+++ Practice/src/Java8Features/ResverseString.java
@@ -0,0 +1,27 @@
+package Java8Features;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class ResverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Saagiruddin Akhtar";
+		String ModifiedStr = str.replace(" ","");
+		
+		
+		String revStr = ModifiedStr.chars()
+						.mapToObj(c -> (char)c)
+						.collect(Collectors.collectingAndThen(
+								Collectors.toList(),list ->{
+									Collections.reverse(list);
+									
+									return list.stream().map(String :: valueOf).collect(Collectors.joining());
+								}));
+		
+		System.out.println(revStr);
+		
+	}
+
+}
--- Practice/src/com/akhtar/practice1/testing.java
+++ Practice/src/com/akhtar/practice1/testing.java
@@ -8,6 +8,14 @@ public class testing {
 		System.out.println("ehy it ti running slow");
 		System.out.println("again test");
 		System.out.println("thids is final tesint");
+		
+		String s1 = "Sagir";
+		String s2 = new String("Sagir");
+		String s3 = new String("Sagir");
+		System.out.println(s1.equals(s2));
+		System.out.println(s1== s2);
+		System.out.println(s3.equals(s2));
+		System.out.println(s3 == s2);
 	}
 
 }
