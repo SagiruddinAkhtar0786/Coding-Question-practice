2e96017fee4315d34cc066802378811ff66b1062
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-04 23:57:59 +0530

    commit

---
+++ Practice/src/java8Features/DistinctCharcterAndTheirCount.java
@@ -0,0 +1,29 @@
+package java8Features;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class DistinctCharcterAndTheirCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "asdfgASDFGasdfg";
+
+		System.out.println("=======charcterCountUsingJava8 ========");
+
+		charcterCountUsingJava8(str);
+
+	}
+
+	private static void charcterCountUsingJava8(String str) {
+		Map<Character, Long> charCount = str.chars() // convert string to intStream
+				.mapToObj(ch -> (char) ch) // Converts each ASCII value into a Character object.
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		// groupingBy(Function.identity()) → Groups by the character itself.
+		// Collectors.counting() → Counts the occurrences of each character.
+
+		System.out.println(charCount);
+	}
+}
