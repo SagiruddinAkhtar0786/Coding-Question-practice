3484cdceb8f136741d8eab6eccfd76d80c086cb4
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-05 12:25:27 +0530

    commit

--- Practice/src/digitalocean/com/java/programming/interview/questions/ExampleUsingLamdaExpression.java
+++ Practice/src/digitalocean/com/java/programming/interview/questions/ExampleUsingLamdaExpression.java
@@ -5,7 +5,7 @@ public class ExampleUsingLamdaExpression {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Runnable r1 = () -> System.out.println("this is runnable in :: "+Thread.currentThread().getName() +" Thread");
-		r1.run();
+	//	r1.run();
 		
 		// using below code runnning in separate cal stack
 		Thread t1 = new Thread(r1);
