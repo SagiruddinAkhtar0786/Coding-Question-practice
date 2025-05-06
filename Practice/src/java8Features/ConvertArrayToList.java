1273dbb7089945ae6e103d29418ef674c1c7e37a
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-06 11:40:55 +0530

    commit

--- Practice/src/java8Features/ConvertArrayToList.java
+++ Practice/src/java8Features/ConvertArrayToList.java
@@ -3,6 +3,8 @@ package java8Features;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class ConvertArrayToList {
 
@@ -15,6 +17,21 @@ public class ConvertArrayToList {
 		
 		List<Integer> lst = Arrays.stream(intArr).boxed().collect(Collectors.toList());
 		System.out.println("int to list :: "+lst);
+		
+		//List<Integer> stremOf = Stream.of(intArr)	.mapToObj(i -> i).collect(Collectors.toList());
+		
+		List<Integer> lint = IntStream.of(intArr)  // or Arrays.stream(intArr)
+                .mapToObj(Integer::valueOf)
+                .collect(Collectors.toList());
+		System.out.println(lint);
+		
+		
+		List<Integer> lint1 = IntStream.of(intArr)
+                .mapToObj(i -> i)
+                .collect(Collectors.toList());
+		
+		System.out.println(lint1);
+
 
 	}
 
