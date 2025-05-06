c367253b639bcd3efcbb4a61cfb7e61fb1fc047e
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-06 11:14:51 +0530

    commit

--- Practice/src/com/akhtar/practice1/testing.java
+++ Practice/src/com/akhtar/practice1/testing.java
@@ -1,5 +1,9 @@
 package com.akhtar.practice1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class testing {
 
 	public static void main(String[] args) {
@@ -16,6 +20,23 @@ public class testing {
 		System.out.println(s1== s2);
 		System.out.println(s3.equals(s2));
 		System.out.println(s3 == s2);
+		
+		
+		//***********************************************************
+		
+		//convert array to list
+		
+		String [] str = {"sagir", "salim","raju","rajnish"};
+		
+		
+		List<String> lst1 = new ArrayList<String>(Arrays.asList(str));
+		System.out.println("arrays as List : "+lst1);
+		List<String> lst = new ArrayList<String>();
+		
+		for(String s : str)
+			lst.add(s);
+		
+		System.out.println(lst);
 	}
 
 }
