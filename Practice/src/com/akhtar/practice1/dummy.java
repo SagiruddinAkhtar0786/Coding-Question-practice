0546ff92abb429c04ceab17eb16310b7115c686c
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-08 23:43:56 +0530

    commit

--- Practice/src/com/akhtar/practice1/dummy.java
+++ Practice/src/com/akhtar/practice1/dummy.java
@@ -2,10 +2,10 @@ package com.akhtar.practice1;
 
 public class dummy {
 	public static void main(String[] args) {
-		/*//Java LinkedList class can contain duplicate elements.
+		/*Java LinkedList class can contain duplicate elements.
 		Java LinkedList class maintains insertion order.
 		Java LinkedList class is non synchronized.
 		In Java LinkedList class, manipulation is fast because no shifting needs to occur.
-		Java LinkedList class can be used as a list, stack or queue.
-	}*/
+		Java LinkedList class can be used as a list, stack or queue.*/
+	}
 }
