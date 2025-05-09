aa57834ade85660891aca629a24c06c53cb57be1
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-09 23:33:36 +0530

    commit

---
+++ Practice/src/digitalocean/com/java/programming/interview/questions/IterateMap.java
@@ -0,0 +1,25 @@
+package digitalocean.com.java.programming.interview.questions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class IterateMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<Integer,String> map = new HashMap<>();
+		map.put(1, "sagir");
+		map.put(2, "saharsh");
+		map.put(3, "rahu");
+		
+		Set<Entry<Integer,String>> entry = map.entrySet();
+		
+		for(Entry<Integer,String> ele : entry) {
+			System.out.println(ele.getKey() +" : "+ele.getValue());
+		}
+
+	}
+
+}
