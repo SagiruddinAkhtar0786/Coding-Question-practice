7bd1eac06894604236e0e42899854ad9cd879a61
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-09 23:21:52 +0530

    commit

--- Practice/src/com/akhtar/practice1/dummy.java
+++ Practice/src/com/akhtar/practice1/dummy.java
@@ -7,5 +7,30 @@ public class dummy {
 		Java LinkedList class is non synchronized.
 		In Java LinkedList class, manipulation is fast because no shifting needs to occur.
 		Java LinkedList class can be used as a list, stack or queue.*/
+		
+		/*HashSet stores the elements by using a mechanism called hashing.
+		HashSet contains unique elements only.
+		HashSet allows null value.
+		HashSet class is non synchronized.
+		HashSet doesn't maintain the insertion order. Here, elements are inserted on
+		the basis of their hashcode.
+		HashSet is the best approach for search operations
+		*/
+		/*
+		 * Java LinkedHashSet class contains unique elements only like HashSet. Java
+		 * LinkedHashSet class provides all optional set operation and permits null
+		 * elements. 
+		 * Java LinkedHashSet class is non synchronized. 
+		 * Java LinkedHashSet class maintains insertion order.
+		 * 
+		 */
+		
+		
+		/*
+		 * A map contains values on the basis of key, i.e. key and value pair. Each key
+		 * and value pair is known as an entry. A Map contains unique keys. A Map is
+		 * useful if you have to search, update or delete elements on the basis of a
+		 * key.
+		 */
 	}
 }
