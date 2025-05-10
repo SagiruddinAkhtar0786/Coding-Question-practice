02faef0a7b929f251782a36ae12f5bdbe5b1098c
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-10 18:45:17 +0530

    commit

--- Practice/src/digitalocean/com/java/programming/interview/questions/AsccendingOrderAndDescByValue.java
+++ Practice/src/digitalocean/com/java/programming/interview/questions/AsccendingOrderAndDescByValue.java
@@ -1,5 +1,6 @@
 package digitalocean.com.java.programming.interview.questions;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -15,14 +16,16 @@ public class AsccendingOrderAndDescByValue {
 	 map.put(2, "rahul");
 	 map.put(3, "sunny");
 	 map.put(4, "irfan");
+	 map.put(4, "samo");  // we cacn not have the duplicate key but can have the dublicacte value
 	 
 	 Map<Integer,String> comByVal = map
 			 .entrySet()
 			 .stream()
 			 .sorted(Map.Entry.comparingByValue())
-			 .collect(Collectors.toMap(Map.Entry :: getKey, Map.Entry :: getValue ,(oldVlaue,newValue)-> oldVlaue,LinkedHashMap :: new));
+			 .collect(Collectors.toMap(Map.Entry :: getKey, Map.Entry :: getValue ,(oldv,newv)->oldv,LinkedHashMap :: new));
 	 
 	 comByVal.forEach((k,v )-> System.out.println(k +" : "+v));
+	 System.out.println("************************************************************");
 	 
 	 // normal get key and get value
 	  Map<Integer,String> comByValM = map
@@ -37,6 +40,33 @@ public class AsccendingOrderAndDescByValue {
 					 );
 	 
 	 comByValM.forEach((k,v )-> System.out.println(k +" : "+v));
+	 System.out.println("************************************************************");
+
+	 
+	 // normal get key and get value
+	  Map<Integer,String> comByValreverese = map
+			 .entrySet()
+			 .stream()
+			 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+			 .collect(Collectors.toMap(
+					 entry -> entry.getKey(),
+					 entry -> entry.getValue(),
+					 (oldValue,newValue) -> oldValue,
+					 () -> new LinkedHashMap<>())
+					 );
+	 
+	  comByValreverese.forEach((k,v )-> System.out.println(k +" : "+v));
+	  
+		 System.out.println("************************************************************");
+
+	 
+	 map
+	 .entrySet()
+	 .stream()
+	 .sorted(Map.Entry.comparingByValue()).forEach(entry -> System.out.println(entry.getKey() + " : "+ entry.getValue()));
+		 
+		 System.out.println("************************************************************");
+
 	  
 	 
 	 
