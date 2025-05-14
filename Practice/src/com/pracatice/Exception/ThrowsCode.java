b5a3e733da80e708277e0562093f77c921873eab
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-15 00:02:30 +0530

    commit

--- Practice/src/com/pracatice/Exception/ThrowsCode.java
+++ Practice/src/com/pracatice/Exception/ThrowsCode.java
@@ -1,21 +1,31 @@
 package com.pracatice.Exception;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class ThrowsCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			boolean isExist = findFile();
+			 findFile("D:\\GitRepository\\Practice\\ThrowsCode.text");
 		}catch(FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
+		System.out.println("rest of the code.....");
 	}
 
-	private static boolean findFile() {
+	
+
+	private static boolean findFile(String Filelocation) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		return false;
+		FileReader file  = new FileReader(Filelocation);
+		BufferedReader bf = new BufferedReader(file);
+		
+		throw new FileNotFoundException();
+		//return false;
 	}
 
 }
