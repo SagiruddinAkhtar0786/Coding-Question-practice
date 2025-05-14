a5a7634c69d965da2d7343a4e7005fed570c0896
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-14 22:25:40 +0530

    commit

--- Practice/src/com/pracatice/Exception/unChecked/NestedTryBlock.java
+++ Practice/src/com/pracatice/Exception/unChecked/NestedTryBlock.java
@@ -4,22 +4,22 @@ public class NestedTryBlock {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		try {
 			int a = 10;
 			int b = 0;
-			
-			int result = a/b;
-			
+
+			int result = a / b;
+
 			try {
 				int arr[] = new int[3];
 				arr[4] = 10;
-			}catch(ArrayIndexOutOfBoundsException e) {
+			} catch (ArrayIndexOutOfBoundsException e) {
 				e.printStackTrace();
-				System.out.println("Innner Exception :: "+e.getMessage());
+				System.out.println("Innner Exception :: " + e.getMessage());
 			}
-		}catch(ArithmeticException e) {
-			System.out.println("outer exception :: "+e.getMessage());
+		} catch (ArithmeticException e) {
+			System.out.println("outer exception :: " + e.getMessage());
 		}
 
 	}
