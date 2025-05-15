6584e59ac6f45fcdf5e5e89230551a68c6420eb8
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-15 11:49:28 +0530

    commit

--- Practice/src/com/pracatice/Exception/CustomExceptionTesting.java
+++ Practice/src/com/pracatice/Exception/CustomExceptionTesting.java
@@ -5,20 +5,36 @@ class generateException extends Exception{
 	generateException(String msg){
 		super(msg);
 	}
+	
+	generateException(){
+		
+	}
 }
 public class CustomExceptionTesting {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws generateException  {
 		
 		// TODO Auto-generated method stub
 		
-		try {
+	
 			int result = 10/0;
+			//throw new ArithmeticException();
+			throw new generateException("This is user Defined Exception ");
+			
+			/*
+			 output will be arithmetic exception,Exception in thread "main" java.lang.ArithmeticException: / by zero
+	at com.pracatice.Exception.CustomExceptionTesting.main(CustomExceptionTesting.java:20)
+
+user Defined exception will not execute 
+since result = 10/0 will immidiately throe an exception due to which it will not readch to 
+custom exception and we know that once exception occred , normal flow of the execution will inrterrrupt
+
+like if age  is less the 18, at that instane we can use ustom exception ,we cacn not use tha custom exception 
+if already exception catch by JVM which comes for pre defined exception
+			 */
 			
 			
-		}catch(ArithmeticException e) {
-			System.out.println("Divide by zero not applicable :: "+e.getMessage());
-		}
+		
 
 	}
 
