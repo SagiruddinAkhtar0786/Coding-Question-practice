ecd118ed333c9593485a6b1432f6361ee1ceb6fc
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-17 21:01:58 +0530

    commit

--- Practice/src/com/thread/practice/ThreadMethodsAndConstructors/joinMethod.java
+++ Practice/src/com/thread/practice/ThreadMethodsAndConstructors/joinMethod.java
@@ -2,33 +2,47 @@ package com.thread.practice.ThreadMethodsAndConstructors;
 
 class JoinMethodTesting extends Thread {
 //	Thread mainThread;
+	static Thread mainThread;
 
 	@Override
 	public void run() {
-
 		for (int i = 0; i < 5; i++)
 			System.out.println("Thread without Yield :: " + Thread.currentThread());
+		
+		 System.out.println(Thread.currentThread().getName() + " has finished work.");
 	}
 }
 
-public class joinMethod  {
+public class joinMethod {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// mainThread = Thread.currentThread();
 
 		JoinMethodTesting JMT = new JoinMethodTesting();
 		JMT.start();
-		JMT.setName("sr_Akhtar");
-		//JMT.start();
+		JMT.setName("JMT1");
+
+		JoinMethodTesting JMT2 = new JoinMethodTesting();
+		JMT2.start();
+		JMT2.setName("JMT2");
 
 		try {
-			JMT.join();
-			for (int i = 0; i < 5; i++)
-				System.out.println("Thread without Yield >> " + Thread.currentThread());
 
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+			JMT.join();
+		//	JMT2.join();
+		} catch (Exception de) {
+			de.printStackTrace();
 		}
+		// JMT.start();
+
+		/*
+		 * try { JMT.join(); // mainThread.join(); for (int i = 0; i < 5; i++)
+		 * System.out.
+		 * println(" This loop first executes because srAkhtar thred has join method >> "
+		 * + Thread.currentThread());
+		 * 
+		 * } catch (InterruptedException e) { e.printStackTrace(); }
+		 */
 
 	}
 
