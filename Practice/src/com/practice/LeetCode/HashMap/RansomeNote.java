7bf621419ff87a110ab35ceafeb66fc1836ba26d
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-23 22:43:57 +0530

    true

--- Practice/src/com/practice/LeetCode/HashMap/RansomeNote.java
+++ Practice/src/com/practice/LeetCode/HashMap/RansomeNote.java
@@ -1,5 +1,9 @@
 package com.practice.LeetCode.HashMap;
-
+/*
+ Given two strings ransomNote and magazine, return true if ransomNote can be 
+ constructed by using the letters from magazine and false otherwise.
+ Each letter in magazine can only be used once in ransomNote.
+ */
 import java.util.HashMap;
 import java.util.Map;
 
