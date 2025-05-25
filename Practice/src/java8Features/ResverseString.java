d1c2ef64be48a18ebb8ddfa7669dd2fea8a3002c
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-05-25 22:39:10 +0530

    commit

--- Practice/src/java8Features/ResverseString.java
+++ Practice/src/java8Features/ResverseString.java
@@ -10,8 +10,6 @@ public class ResverseString {
 		// TODO Auto-generated method stub
 		String str = "   Saagiruddin   Akhtar   ";
 		String ModifiedStr = str.replace(" ","");
-		
-		
 		String revStr = ModifiedStr.chars()
 						.mapToObj(c -> (char)c)
 						.collect(Collectors.collectingAndThen(
@@ -23,12 +21,12 @@ public class ResverseString {
 		
 		System.out.println(revStr);
 		
-		/*String sa = IntStream.range(0, str.length()).peek(x->System.out.println(x)) //generates a stream of integers from 0 to str.length() like 0,1,2,3,4,,,,,,17
+		String sa = IntStream.range(0, str.length()).peek(x->System.out.println(x)) //generates a stream of integers from 0 to str.length() like 0,1,2,3,4,,,,,,17
 				.mapToObj(i -> str.charAt(str.length()-1-i)) //each indices converts to Object and returns reverse character r a t h k a......S
 				//.map(String :: valueOf) // each charcter converts to String "r" ,"a" ,"t"........."s"
 				.map(n->String.valueOf(n))
 				.collect(Collectors.joining());// each Strig of Charcter joining "rathkar......s"
-		*/
+		System.out.println(sa);
 	}
 
 }
