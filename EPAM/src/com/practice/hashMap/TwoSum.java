2f09117a15b443fa484da52a45a5a3c3c56f0e97
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-06-29 11:31:54 +0530

    comit

--- EPAM/src/com/practice/hashMap/TwoSum.java
+++ EPAM/src/com/practice/hashMap/TwoSum.java
@@ -1,32 +1,15 @@
 package com.practice.hashMap;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	int arr[] = {2,7,11,15};
-	int target = 11;
-	
-	
-	int left = 0;
-	int right = arr.length-1;
-	int index1,index2;
-	while(left < right) {
-		int sum = arr[left] +arr[right];
-		
-		if(sum == target) {
-			index1 = left+1;
-			index2 = right+1;
-			System.out.println(index1+","+index2);
-			return;
-		}else if(sum < target) {
-			left++;
-		
-		}else {
-			right--;
-		}
-	}
-	
+	int target = 9;
 	
 	int index[] = findTwoSum(arr,target);
 	if(index.length != 0) {
@@ -35,6 +18,37 @@ public class TwoSum {
 		System.out.println("No index found");
 	}
 	
+	// if not sorted
+	
+	int indexArr[] = findTwoSumUsingMap(arr,target);
+	System.out.println("indexArr :: "+Arrays.toString(indexArr));
+	for(int i = 0 ; i < indexArr.length;i++) {
+		System.out.println("right >> "+indexArr[i]);
+		
+	}
+	if(indexArr.length != 0) {
+		System.out.println("wrong >> "+index[0]);
+		System.out.println("wrong >> "+index[1]);
+	}else {
+		System.out.println("No index found");
+	}
+	
+	}
+
+	private static int[] findTwoSumUsingMap(int[] arr, int target) {
+		// TODO Auto-generated method stub
+		Map<Integer,Integer> map = new HashMap<>();
+		int indexArr[] = new int[2];
+		for(int i = 0 ; i < arr.length ; i++) {
+			if(map.containsKey( target- arr[i])) {
+				indexArr[0] = map.get(target-arr[i]);
+				indexArr[1] = i;
+				
+			}
+			
+			map.put(arr[i], i);
+		}
+		return indexArr;
 	}
 
 	private static int[] findTwoSum(int[] arr, int target) {
