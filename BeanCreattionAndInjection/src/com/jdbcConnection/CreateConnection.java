89463d3f1be3ed4b9c158e42a7524000af231f43
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-07-23 09:55:52 +0530

    commit

---
+++ BeanCreattionAndInjection/src/com/jdbcConnection/CreateConnection.java
@@ -0,0 +1,31 @@
+package com.jdbcConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateConnection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("dfghjkl");
+		
+		Connection con=null;
+		Statement st = null;
+		
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ACID","root","123456");
+			st = con.createStatement();
+			
+			st.executeUpdate("Update ACCOUNTS set balance = balance - 100 WHERE ID=1");
+			con.commit();
+			
+			System.out.println("transaction successfully");
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
